c617c2a8abf057d152f97e6388d9b87beb45c309
Meehdi <elmehdi.kzadri@gmail.com>
2016-04-15 15:08:58 +0200

    LoginServiceTest

--- src/test/java/dcll/LoginServiceTest.java
+++ src/test/java/dcll/LoginServiceTest.java
@@ -1,37 +1,57 @@
 package dcll;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by elmk93 on 15/04/16.
  */
 public class LoginServiceTest {
 
+    private LoginService loginService;
+    String[] str = {"Mehdi"};
+
     @Before
     public void setUp() throws Exception {
-
+        loginService = new LoginService(str);
     }
 
     @Test
     public void testLoginExists() throws Exception {
+        Assert.assertEquals(true, loginService.loginExists("Mehdi"));
+        Assert.assertEquals(false, loginService.loginExists("Kzadri"));
 
     }
 
     @Test
     public void testAddLogin() throws Exception {
-
+        Assert.assertEquals(false, loginService.loginExists("Kzadri"));
+        loginService.addLogin("Kzadri");
+        Assert.assertEquals(true, loginService.loginExists("Kzadri"));
     }
 
     @Test
     public void testFindAllLoginsStartingWith() throws Exception {
-
+        loginService.addLogin("Kzadri");
+        List<String> list = new ArrayList<String>();
+        list = loginService.findAllLoginsStartingWith("M");
+        Assert.assertEquals(true, list.contains("Mehdi"));
+        Assert.assertEquals(false, list.contains("Kzadri"));
     }
 
     @Test
     public void testFindAllLogins() throws Exception {
+
+        List<String> list = new ArrayList<String>();
+        list = loginService.findAllLogins();
+        Assert.assertEquals(true, list.contains("Mehdi"));
+        Assert.assertEquals(false,
+                (list.contains("Mehdi")&&(list.contains("Kzadri"))));
+
 
     }
 }
